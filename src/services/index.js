e94bfb515f158be5941ff691a8c1b8badcf4eb6c
Liviu <liviutoma946@gmail.com>
2023-10-16 22:16:20 +0300

    renamed query

--- src/services/index.js
+++ src/services/index.js
@@ -4,7 +4,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
   const query = gql`
-    query MyQuery {
+    query PostsQuery {
       postsConnection {
         edges {
           node {
