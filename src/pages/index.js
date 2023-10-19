042ad7cd6b7f12fdd8296c26d8650355b6738951
Liviu <liviutoma946@gmail.com>
2023-10-19 22:01:03 +0300

    fetching posts and passing them to post card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,20 @@
 import Categories from '@/components/Categories';
-import Navbar from '@/components/Navbar';
 import PostCard from '@/components/PostCard';
 import PostWidget from '@/components/PostWidget';
+import { getPosts } from '@/services';
 import Head from 'next/head';
 
-const posts = [
-  { title: 'testing', excerpt: 'Learn Next.js' },
-  { title: 'testing', excerpt: 'Learn Next.js' },
-  { title: 'testing', excerpt: 'Learn Next.js' },
-  { title: 'testing', excerpt: 'Learn Next.js' },
-  { title: 'testing', excerpt: 'Learn Next.js' },
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
         <title>Lee's blog</title>
       </Head>
-      <Navbar />
+      <div className='background-container'></div>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='col-span-1 lg:col-span-8'>
           {posts.map((post, i) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.node.title} />
           ))}
         </div>
         <div className='col-span-1 lg:col-span-4'>
@@ -34,4 +26,12 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
 }
