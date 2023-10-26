02260914c9d00513abece5386bca7a7e61bfaf3f
Liviu <liviutoma946@gmail.com>
2023-10-26 23:51:50 +0300

    added get similar posts query

--- src/services/index.js
+++ src/services/index.js
@@ -58,3 +58,37 @@ export const getRecentPosts = async () => {
 
   return response.posts;
 };
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+};
+
+export const getCategories = async () => {
+  const query = gql`
+        query getCategories {
+            categories: {
+                name
+                slug
+            }
+        }
+    `;
+
+  const result = await request(graphqlAPI, query);
+};
