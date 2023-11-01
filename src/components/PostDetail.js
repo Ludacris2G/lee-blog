623bf6ec3fbbcc1af6d96dd86d1d73be30d828d0
Liviu <liviutoma946@gmail.com>
2023-11-01 20:24:29 +0200

    defined getContentFragment function | rendering result

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,7 +1,61 @@
 import moment from 'moment';
+import React from 'react';
 
 const PostDetail = ({ post }) => {
-  console.log(post);
+  const getContentFragment = (index, text, obj, type) => {
+    let modifiedText = text;
+
+    if (obj) {
+      if (obj.bold) {
+        modifiedText = <b key={index}>{text}</b>;
+      }
+      if (obj.italic) {
+        modifiedText = <em key={index}>{text}</em>;
+      }
+      if (obj.underline) {
+        modifiedText = <u key={index}>{text}</u>;
+      }
+    }
+
+    switch (type) {
+      case 'heading-three':
+        return (
+          <h3 key={index} className='text-xl font-semibold mb-4'>
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h3>
+        );
+      case 'paragraph':
+        return (
+          <p key={index} className='mb-8'>
+            {modifiedText.map((item, i) => (
+              <>{item}</>
+            ))}
+          </p>
+        );
+      case 'code-block':
+        return (
+          <pre key={index} className='bg-gray-200 p-4'>
+            <code>{modifiedText}</code>
+          </pre>
+        );
+      case 'block-quote':
+        return (
+          <blockquote
+            key={index}
+            className='border-l-4 pl-4 italic text-gray-600'
+          >
+            {modifiedText.map((item, i) => (
+              <>{item}</>
+            ))}
+          </blockquote>
+        );
+      default:
+        return modifiedText;
+    }
+  };
+
   return (
     <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
       <div className='relative overflow-hidden shadow-md mb-6'>
@@ -43,6 +97,13 @@ const PostDetail = ({ post }) => {
         </div>
       </div>
       <h1 className='mb-8 text-3xl font-semibold'>{post.title}</h1>
+      {console.log(post.content.raw.children)}
+      {post.content.raw.children.map((typeObj, index) => {
+        const children = typeObj.children.map((item, itemIndex) =>
+          getContentFragment(itemIndex, item.text, item)
+        );
+        return getContentFragment(index, children, typeObj, typeObj.type);
+      })}
     </div>
   );
 };
