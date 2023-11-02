6db8a9c0c236e98eacf8ededd76beec4796a67bf
Liviu <liviutoma946@gmail.com>
2023-11-02 17:55:44 +0200

    comment form styling and refs

--- src/components/CommentsForm.js
+++ src/components/CommentsForm.js
@@ -1,7 +1,66 @@
+import { useRef, useState } from 'react';
+
 const CommentsForm = () => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
+  const handleCommentSubmission = () => {
+    setError(false);
+  };
+
   return (
-    <div>
-      <h1>CommentsForm</h1>
+    <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
+      <p className='text-xl mb-8 font-semibold border-b pb-4'>CommentsForm</p>
+      <div className='grid grid-cols-1 gap-4 mb-4'>
+        <textarea
+          ref={commentEl}
+          className='p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700'
+          cols='30'
+          rows='10'
+          placeholder='Leave a comment...'
+          name='comment'
+        />
+      </div>
+      <div className='grid grid-cols-1 gap-4 mb-4 lg:grid-cols-2'>
+        <input
+          type='text'
+          ref={nameEl}
+          className='py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700'
+          placeholder='Name'
+          name='name'
+        />
+        <input
+          type='text'
+          ref={emailEl}
+          className='py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700'
+          placeholder='Email'
+          name='email'
+        />
+      </div>
+      {error && (
+        <p className='text-xs text-red-500 text-center'>
+          All fields are required
+        </p>
+      )}
+      <div className='mt-8 flex flex-col'>
+        <button
+          type='button'
+          onClick={handleCommentSubmission}
+          className='transition duration-500 ease hover:bg-blue-700 inline-block bg-blue-600 text-lg rounded-full text-white px-8 py-3 cursor-pointer'
+        >
+          Post Comment
+        </button>
+        {showSuccessMessage && (
+          <span className='float-right font-semibold mt-3 text-green-500 text-center'>
+            Comment submitted for review
+          </span>
+        )}
+      </div>
     </div>
   );
 };
