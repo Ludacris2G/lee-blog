78a33671a586483ec1b75276fc4451dac4af3107
Liviu <liviutoma946@gmail.com>
2023-11-05 15:45:33 +0200

    removed logs

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -115,7 +115,6 @@ const PostDetail = ({ post }) => {
         </div>
       </div>
       <h1 className='mb-8 text-3xl font-semibold'>{post.title}</h1>
-      {console.log(post.content.raw.children)}
       {post.content.raw.children.map((typeObj, index) => {
         const children = typeObj.children.map((item, itemIndex) =>
           getContentFragment(itemIndex, item.text, item)
