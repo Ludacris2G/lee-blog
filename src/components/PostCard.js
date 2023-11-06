632a69baa5425e1644f30e3f2868db0bf1d30f1d
Liviu <liviutoma946@gmail.com>
2023-11-06 23:54:35 +0200

    post featured img height set to full

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -8,7 +8,7 @@ const PostCard = ({ post }) => {
         <img
           src={post.featuredImage.url}
           alt={post.title}
-          className='object-top absolute w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
+          className='object-top absolute w-full h-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
         />
       </div>
       <h1 className='transition duration-200 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold'>
