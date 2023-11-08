3903495cc164e99157edf08562e99a468047fab7
Liviu <liviutoma946@gmail.com>
2023-11-08 23:15:17 +0200

    logical &&

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,9 +15,10 @@ export default function Home({ posts }) {
       <CarouselSlider />
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='col-span-1 lg:col-span-8'>
-          {posts.map((post, i) => (
-            <PostCard post={post.node} key={post.node.title} />
-          ))}
+          {posts &&
+            posts.map((post, i) => (
+              <PostCard post={post.node} key={post.node.title} />
+            ))}
         </div>
         <div className='col-span-1 lg:col-span-4'>
           <div className='relative top-8 lg:sticky'>
