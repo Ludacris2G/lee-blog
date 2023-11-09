24365979609dc667c5fa68036fcb460294084a9a
Liviu <liviutoma946@gmail.com>
2023-11-09 21:37:08 +0200

    router fallback and styling

--- src/pages/category/[slug].js
+++ src/pages/category/[slug].js
@@ -1,10 +1,33 @@
+import Categories from '@/components/Categories';
+import Loader from '@/components/Loader';
+import PostCard from '@/components/PostCard';
+import PostWidget from '@/components/PostWidget';
 import { getCategories, getCategoryPosts, getPosts } from '@/services';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 
 const CategoryPosts = ({ posts }) => {
-  console.log(posts);
-  return <div>h1</div>;
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
+
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='col-span-1 lg:col-span-8'>
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className='col-span-1 lg:col-span-4'>
+          <div className='relative lg:sticky top-8'>
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryPosts;
