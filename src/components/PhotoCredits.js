a14f9d9a2f38f34b05cf8ed5b30168cf4728d718
Liviu <liviutoma946@gmail.com>
2024-01-26 23:37:10 +0200

    picture credit text color

--- src/components/PhotoCredits.js
+++ src/components/PhotoCredits.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PhotoCredits = () => {
   return (
-    <p className='absolute z-10 bottom-2 left-2 opacity-40 text-[12px]'>photo: @lee.imgs</p>
+    <p className='absolute z-10 bottom-2 left-2 opacity-60 text-[12px] text-white'>photo: @lee.imgs</p>
   )
 }
 
